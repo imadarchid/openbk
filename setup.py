5384e81a2802c4894f040ca20259c5e000c7816f
Imad Archid <imadarchid@gmail.com>
2022-06-20 11:41:18 +0100

    docs(README): adding README path to python setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import find_packages, setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='openbk',
     packages=find_packages(),
-    version='0.0.1',
+    version='0.0.2',
     description='A python library to easily extract information and metrics from Moroccan bank statements.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Imad Archid',
     author_email='hello@imadarchid.com',
     license='GPL',
