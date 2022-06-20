6476978ba6222451bda1ec7eae7a1d19eab98fba
Imad Archid <imadarchid@gmail.com>
2022-06-20 11:33:02 +0100

    fix(extract): Fixing issue with adding method in init

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     version='0.0.1',
     description='A python library to easily extract information and metrics from Moroccan bank statements.',
     author='Imad Archid',
+    author_email='hello@imadarchid.com',
     license='GPL',
     url='https://github.com/imadarchid/openbk',
     install_requires=['tabula-py', 'pandas'],
