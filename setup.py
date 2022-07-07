b1867f326182631e2de42b8bb00e0fe8fcf8a2ca
Imad Archid <imadarchid@gmail.com>
2022-07-07 18:59:54 +0100

    build(setup): updating required packages

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text(encoding="utf8")
 setup(
     name='openbk',
     packages=find_packages(),
-    version='0.0.2',
+    version='1.0.0',
     description='A python library to easily extract information and metrics from Moroccan bank statements.',
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -15,5 +15,6 @@ setup(
     author_email='hello@imadarchid.com',
     license='GPL',
     url='https://github.com/imadarchid/openbk',
-    install_requires=['tabula-py', 'pandas'],
+    install_requires=['tabula-py', 'pandas', 'PyPDF2'],
+    python_requires='>=3.10',
 )
