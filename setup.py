e6cf26e9642b8da9a9c1e5d1f24624ff4c5df576
Imad Archid <imadarchid@gmail.com>
2023-03-08 16:42:23 +0100

    chore: bump version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text(encoding="utf8")
 setup(
     name='openbk',
     packages=find_packages(),
-    version='1.0.0',
+    version='1.0.1',
     description='A python library to easily extract information and metrics from Moroccan bank statements.',
     long_description=long_description,
     long_description_content_type='text/markdown',
